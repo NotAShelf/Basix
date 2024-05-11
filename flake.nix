95e8054cccdb1bfa362d232dc0ad74d1a8624ba6
NotAShelf <raf@notashelf.dev>
2024-05-11 04:54:47 +0300

    expose `evalSchemeData` function

--- flake.nix
+++ flake.nix
@@ -6,7 +6,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {flake-parts, self, ...}:
+  outputs = inputs @ {
+    flake-parts,
+    self,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {pkgs, ...}: {
@@ -28,6 +32,10 @@
           lib.listToAttrs
         ];
       in {
+        lib = {
+          inherit evalSchemeData;
+        };
+
         schemeData = {
           base16 = evalSchemeData "${self}/json/base16";
           base24 = evalSchemeData "${self}/json/base24";
