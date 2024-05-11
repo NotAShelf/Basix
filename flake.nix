edc9911f48b20be17501850f5b953a3393e3be43
NotAShelf <raf@notashelf.dev>
2024-05-11 04:30:52 +0300

    flake: eval scheme data

--- flake.nix
+++ flake.nix
@@ -1,23 +1,35 @@
 {
-  description = "Description for the project";
+  description = "Base16/Base24 schemes for Nix";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    schemes = {
-      url = "github:tinted-theming/schemes";
-      flake = false;
-    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {pkgs, ...}: {
-        # Example if we could make it a list:
-        # builtins.mapAttrs (_: p: builtins.fromJSON (builtins.readFile p)) (import ./list.nix)
         packages = {
           convert-scheme = pkgs.callPackage ./packages/convert-scheme/package.nix {};
+        };
+      };
+
+      flake = let
+        inherit (inputs.nixpkgs) lib;
+        evalSchemeData = x:
+          (lib.mapAttrs' (
+            n: v:
+              lib.optionalAttrs (v == "regular" && lib.hasSuffix ".json" n) {
+                name = lib.removeSuffix ".json" n;
+                value = x + n;
+              }
+          ))
+          (builtins.readDir x);
+      in {
+        schemeData = {
+          base16 = evalSchemeData ./json/base16;
+          base24 = evalSchemeData ./json/base24;
         };
       };
     };
